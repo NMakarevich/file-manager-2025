4aad96521bcbb0db7f9e10148b622f98f9a417ee
NMakarevich <nikoladze529@mail.ru>
2025-05-01 12:55:32 +0300

    feat: add navigation utils

---
+++ src/utils/navigation.js
@@ -0,0 +1,50 @@
+import * as os from "node:os";
+import * as path from "node:path";
+import { access, readdir } from "node:fs/promises";
+import { ERRORS } from "../constants.js";
+
+export let currentDirectory = os.homedir();
+
+export function printCurrentDirectory() {
+  console.log(`You are currently in ${currentDirectory}`);
+}
+
+export async function up() {
+  return await cd("..");
+}
+
+export async function cd(newPath) {
+  try {
+    if (path.isAbsolute(newPath)) {
+      await access(path.parse(newPath).dir);
+      currentDirectory = newPath;
+    } else {
+      await access(path.join(currentDirectory, newPath));
+      currentDirectory = path.join(currentDirectory, newPath);
+    }
+  } catch {
+    console.error(ERRORS.OPERATION_FAILED);
+  }
+}
+
+export async function ls() {
+  try {
+    const dirents = await readdir(currentDirectory, { withFileTypes: true });
+    const directories = dirents
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => ({ Name: dirent.name, Type: "directory" }))
+      .sort(sortByName);
+    const files = dirents
+      .filter((dirent) => dirent.isFile())
+      .map((dirent) => ({ Name: dirent.name, Type: "file" }))
+      .sort(sortByName);
+    console.table([...directories, ...files]);
+  } catch (error) {
+    console.log(error);
+    console.log(ERRORS.OPERATION_FAILED);
+  }
+}
+
+function sortByName(a, b) {
+  return a.Name > b.Name ? 1 : a.Name < b.Name ? -1 : 0;
+}
